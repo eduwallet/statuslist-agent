90127996dab30825930fdd37a16466bc7474b81b
Michiel Uitdehaag <michiel@muisit.nl>
2025-07-02 10:37:55 +0200

    Using the name field of the local.key to provide the did:web identifier

--- src/server/endpoints/getDidSpec.ts
+++ src/server/endpoints/getDidSpec.ts
@@ -1,11 +1,11 @@
 import { Factory } from '@muisit/cryptokey';
-import { getKey } from '../../utils/keymanager';
+import { getKey, getDID } from '../../utils/keymanager';
 import { Request, Router } from 'express'
 
 export function getDidSpec(router:Router) {
     var path = '/.well-known/did.json';
     router!.get(path, async (req: Request, res) => {
-        const key = getKey();   
-        return res.json(await Factory.toDIDDocument(key!));
+        const key = getKey();
+        return res.json(await Factory.toDIDDocument(key!, getDID()));
     });
 }
--- src/utils/keymanager.ts
+++ src/utils/keymanager.ts
@@ -8,11 +8,14 @@ interface KeyData {
 }
 
 let _key:CryptoKey|null = null;
+let _did:string = '';
 
 export async function loadKey()
 {
   const object = JSON.parse(fs.readFileSync('local.key', 'utf8').toString()) as KeyData;
   _key = await Factory.createFromType(object.type, object.privateKeyHex);
+  _did = object.name;
 }
 
 export function getKey() { return _key; }
+export function getDID() { return _did; }
